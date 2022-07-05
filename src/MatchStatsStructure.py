9812d499224d7859f9e71186821b8c673d94fbdb
matteo <teo.migliaccio@gmail.com>
2022-07-05 15:32:41 +0200

    changes

--- src/MatchStatsStructure.py
+++ src/MatchStatsStructure.py
@@ -1,15 +1,17 @@
 class MatchStatusStructure:
     
-    def get_stats(self, arr):
-        match = {
+    match = {} 
+    def __init__(self, arr):
+        self.match = {
             "home team": arr[0],
             "away team": arr[1],
-            "home score": arr[2],
-            "away score": arr[3],
+            "home score": arr[4],
+            "away score": arr[5],
+            "home shots": arr[8],
+            "away shots": arr[9],
+            "home target shots": arr[10],
+            "away target shots": arr[11],
         }
-        return match
-
-
-time = MatchStatusStructure()
-
-time.get_stats(["ciao", "zio"])
+    
+    def get_match(self):
+        return self.match
--- src/dataExtracter.py
+++ src/dataExtracter.py
@@ -1,16 +1,25 @@
 from bs4 import BeautifulSoup as bs
 from h11 import Data
 import requests
+import sys
+sys.path.append("/src")
+from MatchStatsStructure import MatchStatusStructure
 
 class DataExtracter:
     matches_stats = []
     url = "https://understat.com/match/17124"
-
-    def get_stats(self):
+    structure = any
+    
+    def __init__(self):
         response = requests.get(self.url)
         soup = bs(response.content, "html.parser")
         stats = soup.find_all("div", class_="progress-value")        
-        
-some = DataExtracter()
-
-some.get_stats()
+        i = 0
+        while i in range(len(stats)):
+            stats[i] = stats[i].text
+            i += 1
+        self.structure = MatchStatusStructure(stats)
+    
+prova = DataExtracter()
+stampa = prova.structure.get_match()
+print(stampa)
